07b77667fa98bab906a5a9e25cb01ad4ecd2f581
Ben Tefay <btefay@gmail.com>
2018-07-21 10:39:27 +1000

    Add 'Hello, world' to website root

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace spendtracker.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public ActionResult<string> Get()
+        {
+            return "Hello, World!";
+        }
+    }
+}
