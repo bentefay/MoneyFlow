a890c3846ac8ef26c406eceb3fd30ac5cf0bb7ef
Ben Tefay <btefay@gmail.com>
2018-09-02 20:51:51 +1000

    Turn off parcel auto-install

--- make/Utility/Commands/Parcel.cs
+++ make/Utility/Commands/Parcel.cs
@@ -7,12 +7,12 @@ namespace Make.Utility.Commands
     {
         public static EitherAsync<Error, Unit> RunDev(string projectDirectory, string outputDirectory, string cacheDirectory, int logLevel, ExecutionOptions options)
         {
-            return Executable.RunToEither(options, $"{Exe(projectDirectory)} {projectDirectory}/index.html --out-dir {outputDirectory} --cache-dir {cacheDirectory} --log-level {logLevel} --open");
+            return Executable.RunToEither(options, $"{Exe(projectDirectory)} {projectDirectory}/index.html --out-dir {outputDirectory} --cache-dir {cacheDirectory} --log-level {logLevel} --open --no-autoinstall");
         }
 
         public static EitherAsync<Error, Unit> BuildProd(string projectDirectory, string outputDirectory, string cacheDirectory, int logLevel)
         {
-            return Executable.RunToEither($"{Exe(projectDirectory)} build {projectDirectory}/index.html --out-dir {outputDirectory} --cache-dir {cacheDirectory} --log-level {logLevel}");
+            return Executable.RunToEither($"{Exe(projectDirectory)} build {projectDirectory}/index.html --out-dir {outputDirectory} --cache-dir {cacheDirectory} --log-level {logLevel} --no-autoinstall");
         }
 
         private static string Exe(string projectDirectory)
