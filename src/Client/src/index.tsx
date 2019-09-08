26cc0d9c2ee545a918af3594e96c2a7d445f7e6c
Ben Tefay <ben.tefay@stacktracehq.com>
2019-09-09 07:22:07 +1000

    Enable hot loading of components
    Previously was just hot loading reducers

--- src/Client/src/index.tsx
+++ src/Client/src/index.tsx
@@ -4,14 +4,17 @@ import { render } from "react-dom";
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { Authenticate } from './components/authenticate/authenticate';
+import { hot } from 'react-hot-loader';
 import "./components/styles/icons";
 
 const Root = () => (
     <Authenticate />
 );
 
+const EnrichedRoot = hot(module)(Root);
+
 render(
     <Provider store={store}>
-        <Root />
+        <EnrichedRoot />
     </Provider>,
     document.getElementById("root"));
