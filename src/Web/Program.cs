740e8da1b017205baa93a956941860040e1a0c09
Ben Tefay <ben.tefay@stacktracehq.com>
2020-01-05 09:37:42 +1000

    Move Startup logic into Program (no magic)

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
+using Web.Functions;
+using Web.Utils;
+using Web.Utils.Extensions;
+using Web.Utils.Serialization.Serializers;
 
 namespace Web
 {
@@ -11,16 +17,19 @@ namespace Web
     {
         public static int Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration()
+            var logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
                 .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
                 .Enrich.FromLogContext()
                 .WriteTo.Console()
+                .WriteTo.Seq("http://localhost:5002")
                 .CreateLogger();
+
+            Log.Logger = logger;
 
             try
             {
-                CreateHostBuilder(args)
+                CreateHostBuilder(args, logger)
                     .Build()
                     .Run();
 
@@ -28,24 +37,53 @@ namespace Web
             }
             catch (Exception ex)
             {
-                Log.Fatal(ex, "Host terminated unexpectedly");
+                logger.Fatal(ex, "Host terminated unexpectedly");
                 return 1;
             }
             finally
             {
                 Log.CloseAndFlush();
             }
-
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(string[] args, ILogger logger) =>
             Host
                 .CreateDefaultBuilder(args)
                 .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder
-                        .UseStartup<Startup>();
+                        .ConfigureServices((context, services) =>
+                        {
+                            var config = context.Configuration;
+                            services
+                                .AddSingleton(logger)
+                                .AddSingleton(config.GetStorageConnectionString())
+                                .AddControllers(builder =>
+                                    builder.Filters.Add<SerilogMvcLoggingAttribute>())
+                                .AddNewtonsoftJson(setup => setup
+                                    .SerializerSettings
+                                    .ConfigureForApiControllers(logger));
+                        })
+                        .Configure((context, app) =>
+                        {
+                            var env = context.HostingEnvironment;
+                            app
+                                .InDevelopment(env, x => x
+                                    .UseDeveloperExceptionPage())
+                                .NotInDevelopment(env, x => x
+                                    .UseHsts())
+                                .NotInDevelopment(env, x => x
+                                    .UseDefaultFiles())
+                                .UseStaticFiles()
+                                .NotInDevelopment(env, x => x
+                                    .UseHttpsRedirection())
+                                .UseSerilogRequestLogging()
+                                .UseRouting()
+                                .UseEndpoints(endpoints => { endpoints.MapControllers(); })
+                                .InDevelopment(env, x => x
+                                    .UseProxyToSpaDevelopmentServer("http://localhost:1234"));
+                        });
                 });
     }
 }
