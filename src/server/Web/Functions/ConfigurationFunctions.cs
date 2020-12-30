6b3dd9fbbb5ba3433899a8a731bbffe454f696f7
Ben Tefay <ben.tefay@stacktracehq.com>
2020-12-30 12:29:18 +1000

    Assert connection string is not empty

--- src/server/Web/Functions/ConfigurationFunctions.cs
+++ src/server/Web/Functions/ConfigurationFunctions.cs
@@ -1,6 +1,5 @@
 using System;
 using Microsoft.Extensions.Configuration;
-using Web.Types;
 using Web.Types.Values;
 
 namespace Web.Functions
@@ -8,10 +7,20 @@ namespace Web.Functions
     public static class ConfigurationFunctions
     {
         public static StorageConnectionString GetStorageConnectionString(this IConfiguration configuration) =>
-            new StorageConnectionString(configuration.GetRequiredValue<string>("StorageConnectionString"));
+            new StorageConnectionString(configuration.GetNonEmptyValue("StorageConnectionString"));
 
         public static T GetRequiredValue<T>(this IConfiguration configuration, string key) where T : class =>
             configuration.GetValue<T>(key) ?? throw new ConfigurationException($"Configuration is missing required key {key}");
+
+        public static string GetNonEmptyValue(this IConfiguration configuration, string key)
+        {
+            var value = configuration.GetRequiredValue<string>(key);
+
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ConfigurationException($"Configuration key {key} has empty or whitespace value");
+
+            return value;
+        }
     }
 
     public class ConfigurationException : Exception
